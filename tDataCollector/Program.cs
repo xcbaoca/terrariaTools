c85a870a0b671a5e00fac872b348bfbad6c94205
frog_6 <xcbaoca@163.com>
2024-11-06 16:36:43 +0800

    将C#项目设置为FrameWork，以便使用第三方软件包

--- tDataCollector/Program.cs
+++ tDataCollector/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HtmlAgilityPack;
+using Microsoft.Data.Sqlite;
+using SQLitePCL;
+
+namespace tDataCollector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
